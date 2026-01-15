e92a9f81627e799cd5ee6281512f22aa1bd5c056
kchranger <kchranger@naver.com>
2026-01-15 20:57:48 +0900

    chore: 프로젝트 초기 설정 추가

---
+++ src/main/java/site/one_question/OneQuestionApplication.java
@@ -0,0 +1,13 @@
+package site.one_question;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneQuestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneQuestionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/one_question/OneQuestionApplicationTests.java
@@ -0,0 +1,13 @@
+package site.one_question;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneQuestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
