0339906458f35263347e6415a6811f9e18ba8d02
kchranger <kchranger@naver.com>
2026-02-02 21:09:44 +0900

    refactor: app-version API 엔드포인트 수정

--- src/main/java/site/one_question/global/api/app_version/AppVersionController.java
+++ src/main/java/site/one_question/global/api/app_version/AppVersionController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/app-version")
+@RequestMapping("/api/v1/app-versions")
 public class AppVersionController implements AppVersionApi {
 
   @Value("${app.version.min}")
