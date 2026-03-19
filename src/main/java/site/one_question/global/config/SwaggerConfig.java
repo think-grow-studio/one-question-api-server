aa26f8dd01bba8c0aa3c1fcf49135af7f2f5f2e6
kchranger <kchranger@naver.com>
2026-03-19 21:57:32 +0900

    docs: swagger 웹에 Bearer 토큰 추가 설정 적용

--- src/main/java/site/one_question/global/config/SwaggerConfig.java
+++ src/main/java/site/one_question/global/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package site.one_question.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +18,15 @@ public class SwaggerConfig {
                         .title("One Question API")
                         .version("v1.0.0")
                         .description("One Question 서비스 API 문서")
+                )
+                .addSecurityItem(new SecurityRequirement().addList("Bearer"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer", new SecurityScheme()
+                                .name("Bearer")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                        )
                 );
     }
 }
