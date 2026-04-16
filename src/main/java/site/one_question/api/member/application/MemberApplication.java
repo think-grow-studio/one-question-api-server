ebf3350aad1bad9a19759dd2f677371cc8df90fd
kchranger <kchranger@naver.com>
2026-04-16 20:38:21 +0900

    feat: 내 정보 조회시 알람 세팅 응답 필드 추가

--- src/main/java/site/one_question/api/member/application/MemberApplication.java
+++ src/main/java/site/one_question/api/member/application/MemberApplication.java
@@ -7,6 +7,8 @@ import org.springframework.transaction.annotation.Transactional;
 import site.one_question.api.member.domain.Member;
 import site.one_question.api.member.domain.MemberService;
 import site.one_question.api.member.presentation.response.GetMemberResponse;
+import site.one_question.api.notification.domain.QuestionReminderSettingService;
+import site.one_question.api.notification.presentation.response.QuestionReminderSettingResponse;
 import site.one_question.api.question.domain.QuestionCycleService;
 
 @Service
@@ -16,12 +18,17 @@ public class MemberApplication {
 
     private final MemberService memberService;
     private final QuestionCycleService questionCycleService;
+    private final QuestionReminderSettingService questionReminderSettingService;
 
     @Transactional(readOnly = true)
     public GetMemberResponse getMe(Long memberId) {
         Member member = memberService.findById(memberId);
         LocalDate firstCycleStartDate = questionCycleService.getFirstCycle(memberId).getStartDate();
-        return GetMemberResponse.from(member, firstCycleStartDate);
+        QuestionReminderSettingResponse notificationSetting = questionReminderSettingService
+                .findByMember(member)
+                .map(QuestionReminderSettingResponse::from)
+                .orElse(null);
+        return GetMemberResponse.from(member, firstCycleStartDate, notificationSetting);
     }
 
     public Member updateMe(Long memberId, String fullName, String locale) {
