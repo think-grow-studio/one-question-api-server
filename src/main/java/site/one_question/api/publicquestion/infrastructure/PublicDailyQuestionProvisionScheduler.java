9243213fafd744bc59e2bcfabfdb7a2f34655995
kchranger <kchranger@naver.com>
2026-05-23 22:18:00 +0900

    feat: 서버 실행시 public daily question 생성에 대한 로직이 1회 동작하도록 로직 추가

--- src/main/java/site/one_question/api/publicquestion/infrastructure/PublicDailyQuestionProvisionScheduler.java
+++ src/main/java/site/one_question/api/publicquestion/infrastructure/PublicDailyQuestionProvisionScheduler.java
@@ -2,6 +2,8 @@ package site.one_question.api.publicquestion.infrastructure;
 
 import lombok.RequiredArgsConstructor;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import site.one_question.api.publicquestion.application.PublicDailyQuestionProvisionApplication;
@@ -11,6 +13,16 @@ import site.one_question.api.publicquestion.application.PublicDailyQuestionProvi
 public class PublicDailyQuestionProvisionScheduler {
 
     private final PublicDailyQuestionProvisionApplication provisioner;
+
+    @EventListener(ApplicationReadyEvent.class)
+    @SchedulerLock(
+            name = "publicDailyQuestionProvisionScheduler",
+            lockAtLeastFor = "PT1M",
+            lockAtMostFor = "PT10M"
+    )
+    public void provisionPublicDailyQuestionsOnStartup() {
+        provisioner.provision();
+    }
 
     @Scheduled(cron = "0 0 0 * * *", zone = "UTC")
     @SchedulerLock(
