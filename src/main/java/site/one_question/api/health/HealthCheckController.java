78641fc49aeb815732645e86237bd9e5b3c208ad
kchranger <kchranger@naver.com>
2026-06-03 11:31:38 +0900

    chore: Health Check API 로그 시작/종료 로그 추가

--- src/main/java/site/one_question/api/health/HealthCheckController.java
+++ src/main/java/site/one_question/api/health/HealthCheckController.java
@@ -2,11 +2,13 @@ package site.one_question.api.health;
 
 import java.util.HashMap;
 import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/health")
 public class HealthCheckController implements HealthCheckApi {
@@ -14,8 +16,10 @@ public class HealthCheckController implements HealthCheckApi {
     @GetMapping
     @Override
     public ResponseEntity<Map<String, String>> healthCheck() {
+      log.debug("[API] Health Check API 시작");
       Map<String, String> response = new HashMap<>();
       response.put("status", "healthy");
+      log.debug("[API] Health Check API 종료");
       return ResponseEntity.ok(response);
     }
   }
